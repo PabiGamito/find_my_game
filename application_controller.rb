70cf50b2389f1f188534bcb2adce007048342d8c
pabigamito <pablogamito@gmail.com>
2015-07-16 17:44:04 +0000

    all working

--- application_controller.rb
+++ application_controller.rb
@@ -7,17 +7,29 @@ class MyApp < Sinatra::Base
     erb :index
   end
 
-  get '/vote' do
-    erb :under_construction
+	get '/questions' do
+		@qnum = 1
+		erb :questions
   end
 
-#   post '/results'
-#     puts params
-#     @answer= params[:first_answer]
-#     if @answer == "An answer"
-#       @next_page = "./next_page"
-#     elsif condition
-#       @other_page == ""
-#   end
+	post '/questions' do
+		puts params
+		@qnum = params[:qnum].to_i
+		@answer= params[:ans]
+		if @answer == "No" && @qnum == 1
+			@questions = "Next Question"
+			@qnum += 1
+		elsif @answer == "Yes" && @qnum == 1
+			@questions = "Other next question"
+			@qnum += 1
+		elsif @answer == "No" && @qnum == 2
+				@questions = "Hooray!"
+		elsif @answer == "Yes" && @qnum == 2
+			@questions = "Other next question"
+			@qnum += 1
+		end
+		puts @questions
+		erb :questions
+  end
 
 end
