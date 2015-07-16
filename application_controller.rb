5c2545e33af41238a626fca0f12cb48c352af998
PabiGamito <pablogamito@gmail.com>
2015-07-16 09:41:54 -0400

    save-push

--- application_controller.rb
+++ application_controller.rb
@@ -11,4 +11,14 @@ class MyApp < Sinatra::Base
     erb :under_construction
   end
 
+  post '/results'
+    puts params
+    @answer= params[:first_answer]
+    if @answer == "An answer"
+      @next_page = "./next_page"
+    elsif condition
+      @other_page == ""
+    end
+  end
+
 end
