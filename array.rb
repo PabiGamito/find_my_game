d70b2302fb95fb067d7c5134e9d7759b93affa4d
PabiGamito <pablogamito@gmail.com>
2015-07-16 18:10:08 -0400

    Added Stuff

---
+++ array.rb
@@ -0,0 +1,67 @@
+games=["wow", 
+  "minecraft", 
+  "diablo", 
+  "rez", 
+  "monkeyball", 
+  "tetris", 
+  "ikaruga", 
+  "lumines", 
+  "limbo", 
+  "pacman", 
+  "punchout", 
+  "braid", 
+  "portal", 
+  "supersmashbros", 
+  "sims", 
+  "citiesskyline", 
+  "simcity", 
+  "easports", 
+  "mortalkombat", 
+  "mariokart", 
+  "rockband"
+unkown
+unkown]
+
+#Unsure: Katamari Democracy, Foul Calieur, & 3 more
+
+Yes 1 remove:
+["minecraft",  
+  "tetris",  
+  "lumines", 
+  "limbo",  
+  "braid", 
+  "portal", 
+  "supersmashbros", 
+  "sims", 
+  "citiesskyline", 
+  "simcity", 
+  "easports", 
+  "mortalkombat", 
+  "mariokart"]
+  
+  Yes 2 (Unreal) remove:
+  nil
+
+  No 2 (Average) remove:
+  nil
+
+No 1 remove:
+["wow", 
+  "diablo", 
+  "rez", 
+  "monkeyball", 
+  "ikaruga",  
+  "pacman", 
+  "punchout",
+  "rockband"
+unkown
+unkown]
+    Yes 2 remove:
+    nil
+
+    No 2 remove:
+    nil
+
+
+
+
